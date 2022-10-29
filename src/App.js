a11d15057015aa3662387d8d40c0829c6eb0a2a5
a.bubelev <bubeleu@gmail.com>
2022-10-29 13:01:34 +0300

    modal

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
-    </div>
+<div></div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Count from './component/count/count';
+import Modal from './component/modal/modal';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Count/>
+    {/* <Count/> */}
+    <Modal/>
   </React.StrictMode>
 );
 reportWebVitals();

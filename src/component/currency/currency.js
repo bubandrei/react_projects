225badc9b206a8996eccf2d73ada84046a10a193
a.bubelev <bubeleu@gmail.com>
2022-10-30 12:37:01 +0300

    state for currency

--- src/component/currency/currency.js
+++ src/component/currency/currency.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Block } from './block';
 import './currency_style.scss';
 
+const url = 'https://cdn.cur.su/api/latest.json'
+
 function Currency() {
-  return (
-    <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
-    </div>
-  );
+    const [rates, setRates] = useState({});
+    const [fromCurrency, setFromCurrency] = useState('USD')
+    const [toCurrency, setToCurrency] = useState('EUR')
+    useEffect(() => {
+        fetch(url)
+        .then(res => res.json())
+        .then((json) => setRates(json.rates))
+        .catch((err)=> console.log(err))
+    }, []);
+    console.log(rates)
+    return (
+        <div className="App">
+            <Block value={0} currency={fromCurrency} onChangeCurrency={setFromCurrency} />
+            <Block value={0} currency={toCurrency} onChangeCurrency={setToCurrency} />
+        </div>
+    );
 }
 
 export default Currency;

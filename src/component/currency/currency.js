1cd94543fdcce9d696588aeeddcbf2c5592d23ed
a.bubelev <bubeleu@gmail.com>
2022-10-30 13:51:01 +0300

    rates done

--- src/component/currency/currency.js
+++ src/component/currency/currency.js
@@ -1,38 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Block } from './block';
 import './currency_style.scss';
 
 const url = 'https://cdn.cur.su/api/latest.json'
 
 function Currency() {
-    const [rates, setRates] = useState({});
-    const [fromCurrency, setFromCurrency] = useState('PLN')
-    const [toCurrency, setToCurrency] = useState('EUR')
+
+    // const [rates, setRates] = useState({});
+
+    const ratesRef = useRef({});
+
+    const [fromCurrency, setFromCurrency] = useState('USD')
+    const [toCurrency, setToCurrency] = useState('PLN')
     const [valueFrom, setValueFrom] = useState(0);
     const [valueTo, setValueTo] = useState(0);
     useEffect(() => {
         fetch(url)
             .then(res => res.json())
-            .then((json) => setRates(json.rates))
+            .then((json) => {
+                ratesRef.current = json.rates;
+                onChangeFromValue(1)
+            })
             .catch((err) => console.log(err))
     }, []);
-    console.log(rates)
-    console.log(fromCurrency)
-    console.log(rates[fromCurrency])
     const onChangeFromValue = (value) => {
-
         setValueFrom(value)
-        const price = value / rates[fromCurrency];
-        const result = price * rates[toCurrency];
-        setValueTo(result)
+        const price = value / ratesRef.current[fromCurrency];
+        const result = price * ratesRef.current[toCurrency];
+        setValueTo(result.toFixed(2))
     }
     const onChangeToValue = (value) => {
-        const result = (rates[fromCurrency]/rates[toCurrency])*value;
-        setValueFrom(result)
+        const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
+        setValueFrom(result.toFixed(2))
         setValueTo(value)
     }
-    console.log(valueTo)
+    useEffect(() => {
+        onChangeFromValue(valueFrom)
+    }, [fromCurrency, valueFrom]);
 
+    useEffect(() => {
+        onChangeToValue(valueTo)
+    }, [toCurrency, valueTo])
 
     return (
         <div className="App">
